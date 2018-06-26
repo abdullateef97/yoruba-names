c9007876c7a20ecc8e93ce0a2d27b7bc229e803e
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2018-06-26 15:46:45 +0100

    added fetch_names_async and fetch_names_refresh

--- src/actions/names.js
+++ src/actions/names.js
@@ -2,11 +2,28 @@ import {fetch_names, fetch_names_no_loading,
      fetch_names_start, fetch_names_fail} from './types';
 import {fetchNames,search} from '../api/yoruba';
 import {Actions} from 'react-native-router-flux'
-
+import {getNamesAsync, setNamesAsync, getPageAsync, setPageAsync} from '../api/asyncApi'
 
 export const fetch_names_action = (page) => {
     return (dispatch) => {
         fetch_start(dispatch);
+        fetchNames(page).then((data) => {
+            fetch_success(dispatch, data);
+            setNamesAsync(data).then(() => {});
+        }).catch(err => fetch_fail(dispatch,err));
+    }
+}
+
+export const fetch_names_async = () => {
+    return (dispatch) => {
+        getNamesAsync().then((names) => {
+            fetch_success(dispatch, names)
+        }).catch(err =>  fetch_fail(dispatch, err))
+    }
+}
+
+export const fetch_names_refresh = (page) => {
+    return (dispatch) => {
         fetchNames(page).then((data) => {
             fetch_success(dispatch, data);
         }).catch(err => fetch_fail(dispatch,err));
@@ -34,4 +51,3 @@ export const fetch_success = (dispatch, namesArray) => {
     })
 }
 
-export const fetch_names_async = 
